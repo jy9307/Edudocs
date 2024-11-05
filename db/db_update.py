3fe1b51892c8472c11df5f4740c3f7f1c0369db5
jy9307 <genitor@naver.com>
2024-11-05 11:05:16 +0900

    파일 정리

--- db/db_update.py
+++ db/db_update.py
@@ -61,7 +61,7 @@ def milvus_upload(collection_name, docs, drop_old=True) :
     vector_store = Milvus(
     embedding_function=embedder,
     collection_name=collection_name,
-    connection_args={"host": "3.39.234.177", "port": "19530"},  # Separate host and port
+    connection_args={"host": "3.38.106.139", "port": "19530"},  # Separate host and port
     drop_old=drop_old,
 )
 
@@ -98,55 +98,61 @@ def milvus_upload(collection_name, docs, drop_old=True) :
 
 ####--------- Upload work_law
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 500,
-    chunk_overlap = 200,
-    keep_separator = True,
-    separators=[r"\n제"],  # 정규표현식 포함
-    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-)
+# splitter = RecursiveCharacterTextSplitter(
+#     chunk_size = 500,
+#     chunk_overlap = 200,
+#     keep_separator = True,
+#     separators=[r"\n제"],  # 정규표현식 포함
+#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+# )
+
+
+# loaders = [
+#     # 파일을 로드합니다.
+#     TextLoader("./복무규정.txt"),
+# ]
+
+# docs_1 = []  # 빈 리스트를 생성합니다.
+# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+#     docs_1.extend(
+#         loader.load_and_split(text_splitter=splitter)
+#     )  # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
+
+
+# add_metadata(docs_1, "link", "https://www.law.go.kr/%EB%B2%95%EB%A0%B9/%EA%B5%AD%EA%B0%80%EA%B3%B5%EB%AC%B4%EC%9B%90%20%EB%B3%B5%EB%AC%B4%EA%B7%9C%EC%A0%95")
+# add_metadata(docs_1, "law_title", "국가공무원 복무규정")
+
+# loaders = [
+#     # 파일을 로드합니다.
+#     TextLoader("./교육공무원법.txt"),
+# ]
+
+# docs_2 = []  # 빈 리스트를 생성합니다.
+# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+#     docs_2.extend(
+#         loader.load_and_split(text_splitter=splitter)
+#     )  # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
+
+
+# add_metadata(docs_2, "link", "https://www.law.go.kr/%EB%B2%95%EB%A0%B9/%EA%B5%90%EC%9C%A1%EA%B3%B5%EB%AC%B4%EC%9B%90%EB%B2%95")
+# add_metadata(docs_2, "law_title", "교육공무원법")
+
+# docs = docs_1+docs_2
+# milvus_upload("work_law",docs)
 
 
 loaders = [
     # 파일을 로드합니다.
-    TextLoader("./복무규정.txt"),
+    PyPDFLoader("./깊이있는수업.pdf"),
 ]
 
-docs_1 = []  # 빈 리스트를 생성합니다.
+docs = []  # 빈 리스트를 생성합니다.
 for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-    docs_1.extend(
-        loader.load_and_split(text_splitter=splitter)
+    docs.extend(
+        loader.load_and_split()
     )  # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
 
-
-add_metadata(docs_1, "link", "https://www.law.go.kr/%EB%B2%95%EB%A0%B9/%EA%B5%AD%EA%B0%80%EA%B3%B5%EB%AC%B4%EC%9B%90%20%EB%B3%B5%EB%AC%B4%EA%B7%9C%EC%A0%95")
-add_metadata(docs_1, "law_title", "국가공무원 복무규정")
-
-loaders = [
-    # 파일을 로드합니다.
-    TextLoader("./교육공무원법.txt"),
-]
-
-docs_2 = []  # 빈 리스트를 생성합니다.
-for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-    docs_2.extend(
-        loader.load_and_split(text_splitter=splitter)
-    )  # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
-
-
-add_metadata(docs_2, "link", "https://www.law.go.kr/%EB%B2%95%EB%A0%B9/%EA%B5%90%EC%9C%A1%EA%B3%B5%EB%AC%B4%EC%9B%90%EB%B2%95")
-add_metadata(docs_2, "law_title", "교육공무원법")
-
-docs = docs_1+docs_2
-milvus_upload("work_law",docs)
-
-
-
-
-# add_keyword_metadata(docs)
-
-
-
+milvus_upload("deep_lesson",docs)
 
 
 
--- db/manage_collections.py
+++ db/manage_collections.py
@@ -1,7 +1,7 @@
 from pymilvus import connections, Collection, utility 
 
 # Milvus 서버에 연결
-connections.connect("default", host="3.39.234.177", port="19530")  # Milvus 서버의 host와 port에 맞게 수정
+connections.connect("default", host="3.38.106.139", port="19530")  # Milvus 서버의 host와 port에 맞게 수정
 
 collections = utility.list_collections() 
 # 결과 출력 
