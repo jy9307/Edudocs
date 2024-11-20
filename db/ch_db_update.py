1624471d720a087a97c43de0f4dab53d206072c4
jy9307 <genitor@naver.com>
2024-11-20 22:49:55 +0900

    주석 제거

--- db/ch_db_update.py
+++ db/ch_db_update.py
@@ -11,36 +11,36 @@ from langchain_chroma import Chroma
 
 #-----------------------------------------------------규정
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n제"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n제"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("교육공무원법.txt"),
-#     TextLoader("복무규정.txt")
-# ]
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("교육공무원법.txt"),
+    TextLoader("복무규정.txt")
+]
 
-# docs = []  # 빈 리스트를 생성합니다.
+docs = []  # 빈 리스트를 생성합니다.
 
 
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )  
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )  
 
-# # 저장할 경로 지정
-# DB_PATH = "./chroma_db"
+# 저장할 경로 지정
+DB_PATH = "./chroma_db"
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="work_law"
-# )
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="work_law"
+)
 
 
 #-------------------------------------------------------초중등교육법
@@ -78,32 +78,30 @@ persist_db = Chroma.from_documents(
 
 #-------------------------------------------------------성취기준
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 1500,
-#     chunk_overlap = 200,
-#     # keep_separator = True,
-#     # separators=[r"\n초등학교"],  # 정규표현식 포함
-#     # is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 200,
+    # keep_separator = True,
+    # separators=[r"\n초등학교"],  # 정규표현식 포함
+    # is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("./초등 성취기준.txt"),
-# ]
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("./초등 성취기준.txt"),
+]
 
-# docs = []  # 빈 리스트를 생성합니다.
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     ) 
-#      # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
-# # 저장할 경로 지정
-# DB_PATH = "./chroma_db"
+docs = []  # 빈 리스트를 생성합니다.
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    ) 
+     # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
+# 저장할 경로 지정
+DB_PATH = "./chroma_db"
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="achievement_standard"
-# )
-
-#-------------------------------------------------------초중등 교육법
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="achievement_standard"
+)
