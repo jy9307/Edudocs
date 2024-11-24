5c96ce2bbb59c8edbaa912abc173687302cc2052
jy9307 <genitor@naver.com>
2024-11-24 14:44:41 +0900

    db 코드수정

--- db/ch_db_update.py
+++ db/ch_db_update.py
@@ -14,227 +14,227 @@ DB_PATH = "./chroma_db"
 
 #-----------------------------------------------------규정
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n제"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n제"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("work_laws.txt"),
-#     TextLoader("work_laws_for_educator.txt")
-# ]
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("work_laws.txt"),
+    TextLoader("work_laws_for_educator.txt")
+]
 
-# docs = []  # 빈 리스트를 생성합니다.
-
-
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )  
-
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="work_law"
-# )
+docs = []  # 빈 리스트를 생성합니다.
 
 
-# #-------------------------------------------------------초중등교육법
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )  
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n제"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
-
-
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("초중등교육법.txt"),
-# ]
-
-# docs = []  # 빈 리스트를 생성합니다.
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="work_law"
+)
 
 
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )  
+#-------------------------------------------------------초중등교육법
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edu_law"
-# )
-
-
-# #-------------------------------------------------------성취기준
-
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 1500,
-#     chunk_overlap = 200,
-#     # keep_separator = True,
-#     # separators=[r"\n초등학교"],  # 정규표현식 포함
-#     # is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n제"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("./초등 성취기준.txt"),
-# ]
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("초중등교육법.txt"),
+]
 
-# docs = []  # 빈 리스트를 생성합니다.
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     ) 
-
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="achievement_standard"
-# # )
-
-# #-------------------------------------------------------공문
-
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 100,
-#     chunk_overlap = 0,
-#     keep_separator = True,
-#     separators=["기안문"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+docs = []  # 빈 리스트를 생성합니다.
 
 
-# loaders = [
-#     # 파일을 로드합니다.
-#     TextLoader("./official_document.txt"),
-# ]
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )  
 
-# docs = []  # 빈 리스트를 생성합니다.
-# for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )
-#      # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edu_law"
+)
 
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="official_document"
-# )
+#-------------------------------------------------------성취기준
 
-# ####--------- Upload edutech
-
-# loaders = [
-#     TextLoader("./에듀테크 수업 설계안.txt")
-# ]
-
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n\(\d+\)"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 200,
+    # keep_separator = True,
+    # separators=[r"\n초등학교"],  # 정규표현식 포함
+    # is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# docs =[]
-# for loader in loaders :
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("./초등 성취기준.txt"),
+]
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edutech_lesson"
-# )
+docs = []  # 빈 리스트를 생성합니다.
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    ) 
 
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="achievement_standard"
+)
 
-# loaders = [
-#     TextLoader("./에듀테크 종류.txt")
-# ]
+#-------------------------------------------------------공문
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n\(\d+\)"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
-
-
-# docs =[]
-# for loader in loaders :
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )
-
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edutech_collection"
-# )
-
-# ####--------- Upload 깊이있는 수업
-
-# loaders = [
-#     TextLoader("./깊이있는 수업 단원 설계 예시 자료.txt")
-# ]
-
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 500,
-#     chunk_overlap = 200,
-#     keep_separator = True,
-#     separators=[r"\n\(\d+\)"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    keep_separator = True,
+    separators=["기안문"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
 
 
-# docs =[]
-# for loader in loaders :
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )
+loaders = [
+    # 파일을 로드합니다.
+    TextLoader("./official_document.txt"),
+]
 
-# # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="deep_lesson"
-# )
-
-# ####--------- Upload 행발예시
-
-# loaders = [
-#     TextLoader("행발예시.txt")
-# ]
-
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 50,
-#     chunk_overlap = 0,
-#     keep_separator = True,
-#     separators=[r"\d+"],  # 정규표현식 포함
-#     is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
-# )
+docs = []  # 빈 리스트를 생성합니다.
+for loader in loaders:  # loaders 리스트의 각 로더에 대해 반복합니다.
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )
+     # 로더를 사용하여 문서를 로드하고 docs 리스트에 추가합니다.
 
 
-# docs =[]
-# for loader in loaders :
-#     docs.extend(
-#         loader.load_and_split(text_splitter=splitter)
-#     )
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="official_document"
+)
 
-# for d in docs:
-#     lines = d.page_content.splitlines()
-#     d.metadata['영역'] = lines[1].split(":")[1].strip()
-#     d.metadata['수준'] = lines[2].split(":")[1].strip()
-#     print(d)
+####--------- Upload edutech
 
-# persist_db = Chroma.from_documents(
-#     docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="student_feature"
-# )
+loaders = [
+    TextLoader("./에듀테크 수업 설계안.txt")
+]
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n\(\d+\)"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
+
+
+docs =[]
+for loader in loaders :
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )
+
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edutech_lesson"
+)
+
+
+loaders = [
+    TextLoader("./에듀테크 종류.txt")
+]
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n\(\d+\)"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
+
+
+docs =[]
+for loader in loaders :
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )
+
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="edutech_collection"
+)
+
+####--------- Upload 깊이있는 수업
+
+loaders = [
+    TextLoader("./깊이있는 수업 단원 설계 예시 자료.txt")
+]
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 200,
+    keep_separator = True,
+    separators=[r"\n\(\d+\)"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
+
+
+docs =[]
+for loader in loaders :
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )
+
+# 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="deep_lesson"
+)
+
+####--------- Upload 행발예시
+
+loaders = [
+    TextLoader("행발예시.txt")
+]
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 50,
+    chunk_overlap = 0,
+    keep_separator = True,
+    separators=[r"\d+"],  # 정규표현식 포함
+    is_separator_regex=True,  # 정규표현식 사용 가능하도록 설정
+)
+
+
+docs =[]
+for loader in loaders :
+    docs.extend(
+        loader.load_and_split(text_splitter=splitter)
+    )
+
+for d in docs:
+    lines = d.page_content.splitlines()
+    d.metadata['영역'] = lines[1].split(":")[1].strip()
+    d.metadata['수준'] = lines[2].split(":")[1].strip()
+    print(d)
+
+persist_db = Chroma.from_documents(
+    docs, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="student_feature"
+)
 
 ####--------- Upload 과목 누가기록
 
